4ce5fade1a62e0bfa8813171d2b64413f55d5297
Travis <tbird3@wisc.edu>
2021-10-03 19:21:07 -0500

    Basic menus, star data storing, level unlock system

---
+++ Unstable/Assets/LevelSelectButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    [SerializeField] private int levelNumber;
+    private bool isLocked;
+    private int numStars;
+
+    [SerializeField] private Transform starParent;
+    [SerializeField] private GameObject lockOverlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelStarManager manager = GameObject.FindWithTag("StarManager").GetComponent<LevelStarManager>();
+
+        LevelStarManager.LevelStatus status = manager.GetStatusOfLevel(levelNumber);
+        isLocked = !status.isUnlocked;
+        numStars = status.starsEarned;
+
+        UpdateVisuals();
+    }
+
+    // Update is called once per frame
+    public void Clicked()
+    {
+        if (!isLocked)
+            SceneManager.LoadScene(levelNumber + 1);
+        else
+        {
+            //Maybe play lock wiggle animation?
+        }
+    }
+
+    public void UpdateVisuals()
+    {
+        lockOverlay.SetActive(isLocked);
+        ShowStars(); 
+    }
+
+    public void ShowStars()
+    {
+        for (int i = 0; i < numStars; i++)
+            starParent.GetChild(i).gameObject.SetActive(true);
+    }
+}
---
+++ Unstable/Assets/LevelStarManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelStarManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct LevelStatus
+    {
+        public bool isUnlocked;
+        public int starsEarned;
+    }
+
+    [SerializeField] private LevelStatus[] levelStars;
+
+    private void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("StarManager").Length > 1)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public LevelStatus GetStatusOfLevel(int levelNum)
+    {
+        return levelStars[levelNum - 1];
+    }
+
+    public void SetStars(int levelNum, int newStars)
+    {
+        levelStars[levelNum - 1].starsEarned = Mathf.Max(newStars, levelStars[levelNum -1].starsEarned);
+
+        if ((levelStars.Length > levelNum) && (newStars > 0)) //Unlock next level
+            levelStars[levelNum].isUnlocked = true;
+    }
+}
